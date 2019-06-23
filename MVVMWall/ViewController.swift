b4b9576fefb61ec6af03ed8d3fffcd517dd416e4
Praveen <praveen.velanati@sparity.com>
2019-06-23 18:32:30 -0400

    Initial Commit

---
+++ MVVMWall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMWall
+//
+//  Created by praveen velanati on 6/23/19.
+//  Copyright © 2019 praveen velanati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
